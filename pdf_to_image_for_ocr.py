dc3aa892efebab712688578e5e2aedbd570bcf7f
Geetika Sharma <geetika.shrma16@gmail.com>
2020-04-24 07:28:41 -0400

    Revert "v1"
    This reverts commit 332aa08cdcf78080f52abffe5da45bb62a37a04d.

--- pdf_to_image_for_ocr.py
+++ pdf_to_image_for_ocr.py
@@ -3,8 +3,6 @@ from pdf2image import convert_from_path
 def convert_and_save(filename):
 	pages = convert_from_path(filename, 300)
 	for i, page in enumerate(pages):
-		filename = filename.replace("./temp/","")
-		page_name = "./ocr_images/phyto_image.jpg"
-		page.save(page_name, 'JPEG')
+		page.save("./ocr_image/phyto_image.jpg", 'JPEG')
 		if i>0:
 			break
